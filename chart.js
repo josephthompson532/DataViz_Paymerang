6c9ff1fd775f70d32a83091d8648ffb0280d835d
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2021-07-27 14:53:48 -0700

    Created actual json file, utilized d3 to read in data

--- chart.js
+++ chart.js
@@ -1,25 +1,49 @@
-console.log(seriesData)
+var seriesData = []
+var vendorNames = []
 
+d3.json("data.json").then(myjson => {
+    myjson.forEach((dataObject) => {
+    var total = 0;
+    var remittances = dataObject.payment.remittance;
+    if (Array.isArray(remittances)) {
+        remittances.forEach((innerObject) => {
+            total = total + innerObject.amount;
+        })
+    } else {
+        total = total + remittances.amount
+    }
+    dataObject.payment.total_amount = Math.round(total * 100, 2) / 100;
 
-$("#chart").kendoChart({
-    title: {
-        text: "Customer Payments by Vendor"
-    },
-    dataSource: {
-        data: seriesData
-    },
-    series: [{
-        field: "sales"
-    }],
-    valueAxis: [{
-        labels: {
-            format: "{0:C}"
-        }
-    }],
-    categoryAxis: [{
-        categories: vendorNames,
-        labels: {
-            rotation: -45,
-        }
-    }]
-});
+    })
+
+    myjson.forEach((dataObject) => {
+        var mydict = {}
+        mydict["sales"] = dataObject.payment.total_amount
+        vendorNames.push(dataObject.payment.vendor.name)
+        seriesData.push(mydict)
+    })
+
+    $("#chart").kendoChart({
+        title: {
+            text: "Customer Payments by Vendor"
+        },
+        dataSource: {
+            data: seriesData
+        },
+        series: [{
+            field: "sales"
+        }],
+        valueAxis: [{
+            labels: {
+                format: "{0:C}"
+            }
+        }],
+        categoryAxis: [{
+            categories: vendorNames,
+            labels: {
+                rotation: -45,
+            }
+        }]
+    })
+})
+
